14154e7dbdccb1ac72d24af02d1c6b89d83dba7c
芮智鹏 <ruizhipeng001@gmail.com>
2017-09-04 18:34:46 +0800

    compare

--- api_v2.py
+++ api_v2.py
@@ -12,13 +12,26 @@ def list_repositories_v2(registry):
 
 
 def list_tags_v2(registry):
-    imgs = list_repositories_v2(registry)
-    for img in sorted(imgs.keys()):
+    d_imgs = list_repositories_v2(registry)
+    for img in sorted(d_imgs.keys()):
         t = requests.get("{}/v2/{}/tags/list".format(registry, img))
-        imgs[img] = (list(t.json().get("tags", [])))
-    return imgs
+        d_imgs[img] = (list(t.json().get("tags", [])))
+    return d_imgs
+
+
+def compare_v2(registry1, registry2):
+    d_imgs1 = list_tags_v2(registry1)
+    d_imgs2 = list_tags_v2(registry2)
+    pull_img = {}
+    for img in sorted(d_imgs1.keys()):
+        if img in d_imgs2:
+            # IMG which need pull
+            pull_img[img] = [set(d_imgs1[img]) - set(d_imgs2[img])]
+            # IMG which need del
+            # del_list = set(d_imgs2[img]) - set(d_imgs1[img])
+    return pull_img
+
 
 # print(list_repositories_v2("http://hub.bonc:5000"))
-print(list_tags_v2("http://hub.bonc:5000"))
-# print(tree_img_tags_v2("http://hub.bonc:5000"))
-# print(list(map(list_tags_v2, list_repositories_v2("http://hub.bonc:5000"))))
+# print(list_tags_v2("http://hub.bonc:5000"))
+print(compare_v2("http://hub.bonc:5000", "http://hub.bonc:5000"))
