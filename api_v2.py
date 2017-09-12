77731d67becf91076a68bedb57517dafbefd3a50
芮智鹏 <ruizhipeng001@gmail.com>
2017-09-12 18:43:33 +0800

    tuesday

--- api_v2.py
+++ api_v2.py
@@ -1,5 +1,6 @@
 #!/bin/python3.6
 import requests
+# import os
 
 
 def list_repositories_v2(registry):
@@ -22,16 +23,22 @@ def list_tags_v2(registry):
 def compare_v2(registry1, registry2):
     d_imgs1 = list_tags_v2(registry1)
     d_imgs2 = list_tags_v2(registry2)
-    pull_img = {}
+    pull_imgs = {}
     for img in sorted(d_imgs1.keys()):
         if img in d_imgs2:
             # IMG which need pull
-            pull_img[img] = sorted(set(d_imgs1[img]) - set(d_imgs2[img]))
+            pull_imgs[img] = sorted(set(d_imgs1[img]) - set(d_imgs2[img]))
             # IMG which need del
             # del_list = set(d_imgs2[img]) - set(d_imgs1[img])
-    return pull_img
+    return pull_imgs
 
+
+def sync_img(registry1, registry2):
+    d_pulls = compare_v2(registry1, registry2)
+    for i in d_pulls:
+        print("dict[%s]=" % i, dict[i])
 
 # print(list_repositories_v2("http://hub.bonc:5000"))
 # print(list_tags_v2("http://hub.bonc:5000"))
-print(compare_v2("http://hub.bonc:5000", "http://hub.bonc:5000"))
+# print(compare_v2("http://hub.bonc:5000", "http://hub.bonc:5000"))
+print(sync_img("http://hub.bonc:5000", "http://hub.bonc:5000"))
